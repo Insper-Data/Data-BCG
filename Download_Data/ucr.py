2473eb39689ae6698858eeb436f2137b902a1dcb
gustavobsales <gustavobsales@gmail.com>
2020-09-23 21:47:16 -0300

    Acabei a função que puxa dados da UCR

--- Download_Data/ucr.py
+++ Download_Data/ucr.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import re
+import time
 
 def change_names(name):
     if name == 0:
@@ -16,7 +17,64 @@ def change_names(name):
     elif name == 4:
         return "Police Employee (LEOKA) Data"
 
-def get_ucr_links(data_wanted="All"):
+def crawler(link, wait=60):
+
+    if not isinstance(link, str):
+        return "Inserted link should be a string"
+
+    class download_ucr_data():
+        def __init__(self, link):
+            self.driver = webdriver.Chrome()
+            self.link = link
+
+        def enter_page(self):
+            # Open webpage
+            self.driver.get(self.link)
+
+        def click_download(self):
+            # Click download button
+            download_button = self.driver.find_element_by_xpath('//*[@id="quickDownload"]/div/button')
+            download_button.click()
+            self.driver.execute_script("window.scrollTo(0, 200)")
+
+        def click_stata(self):
+            # Click Stata button
+            stata_button = self.driver.find_element_by_xpath('//*[@id="quickDownload"]/div/ul/li[4]/a')
+            stata_button.click()
+
+        def agree(self):
+            # Agree terms and conditions
+            agree_button = self.driver.find_element_by_xpath('//*[@id="termsOfUseModalWrapper"]/div/div/div[3]/a[1]')
+            agree_button.click()
+
+        def login(self):
+            # Log into account
+            email_input = self.driver.find_element_by_xpath('//*[@id="email"]')
+            password_input = bot.driver.find_element_by_xpath('//*[@id="password"]')
+
+            email_input.send_keys('gustavobsales@gmail.com')
+            password_input.send_keys('wKqyAc5MchiihLEhE99r')
+
+            log_in_button = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[1]/div[1]/form/input[4]')
+            log_in_button.click()
+
+        def close(self):
+            # Close Chrome
+            self.driver.close()
+
+    bot = download_ucr_data(link)
+    bot.enter_page()
+    bot.click_download()
+    time.sleep(2)
+    bot.click_stata()
+    time.sleep(2)
+    bot.agree()
+    time.sleep(2)
+    bot.login()
+    time.sleep(wait)
+    bot.close()
+
+def get_ucr_data(data_wanted="All"):
     """
     O texto inserido deve ser uma das seguintes strings:
             Arrests by Age, Sex, and Race, monthly reports \n
@@ -79,14 +137,11 @@ def get_ucr_links(data_wanted="All"):
     links.name = links.name.apply(lambda x: change_names(x))
 
     if data_wanted.lower() == "all":
-        return links
+        [crawler(link) for link in links.link.tolist()]
+        return
 
     links = links[links.name == data_wanted]
 
-    return links
+    [crawler(link) for link in links.link.tolist()]
 
-# Login e senha do site do UCR
-# gustavobsales@gmail.com
-# wKqyAc5MchiihLEhE99r
-
-
+    return
