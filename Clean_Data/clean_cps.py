b7732d84cff718c47ce941094bd92a4614b74151
mittelmax <maxmitteldorf@gmail.com>
2020-10-20 23:25:04 -0300

    arrumei codigo de limpeza da base cps

--- Clean_Data/clean_cps.py
+++ Clean_Data/clean_cps.py
@@ -3,16 +3,14 @@ import os
 # Para carregar um dataframe tão grande devemos utilizar o a capacidade
 # de processamento paralelo do computador. Para isso podemos utilizar
 # o pacote Dask que implementa essa funcionalidade utilizando a mesma
-# sintaxe do Pandas. Para instalar deve se utilizar "pip3 install dask[complete]"
+# sintaxe do Pandas. Para instalar deve-se utilizar "pip3 install dask[complete]"
 from dask import dataframe as dd
+from dask.diagnostics import ProgressBar
+ProgressBar().register()
 
 # Configurando path do projeto
 cwd = os.getcwd()
 project_wd = os.path.dirname(cwd)
-
-# Setting up pandas parameters
-pd.set_option('display.width', 400)
-pd.set_option('display.max_columns', 100)
 
 # Lendo códigos Fips dos estados
 state_fips_wd = os.path.join(project_wd, "Download_Data/Data/fips_state.csv")
@@ -20,37 +18,30 @@ state_fips = pd.read_csv(state_fips_wd)
 state_fips.drop(columns="state", inplace=True)
 state_fips.rename(columns={"fips_code": "STATEFIP", "post_code": "state"}, inplace=True)
 
-# Criando dataframe em branco para armazenar resultados
-df_final = pd.DataFrame()
-df_final = dd.from_pandas(df_final, npartitions=100)
+# Lendo dataframe com anos desejados apenas
+textfilereader = pd.read_csv("raw_data/cps.csv", iterator=True, index_col=[0])
+df = textfilereader.get_chunk(5000)
 
-# Armazenando número do chunk
-n_chunk = 1
+# descobrindo colunas com apenas Nas
+percent_missing = df.isnull().sum() * 100 / len(df)
+missing_value_df = pd.DataFrame({'column_name': df.columns, 'percent_missing': percent_missing})
+missing_value_df = missing_value_df[missing_value_df["percent_missing"]==100]
+missing_cols = list(missing_value_df["column_name"])
 
-# Loop para processar chunks
-for chunk in pd.read_csv("raw_data/cps.csv", chunksize=100000):
-    if n_chunk <= 68:
-        df_i = chunk
+# Removendo anos indesejados
+anos = list(range(2000, 2016))
+dask_df = dd.read_csv("raw_data/cps.csv", assume_missing=True)
+dask_df = dask_df[dask_df.YEAR.isin(anos)]
 
-        # Removing columns with unwanted variables
-        df_i.drop(columns=["ASECWTH", "MISH", "NUMPREC", "METAREA", "ASECFLAG", "REGION", "HEATSUB", "HEATVAL"], inplace=True)
+# Removendo colunas com apenas Nas
+dask_df = dask_df.drop(missing_cols, axis=1)
 
-        # Removing columns without data
-        df_i.dropna(axis=1, inplace=True)
+# Corrigindo Income pela inflação
+dask_df["HHINCOME"] = dask_df["HHINCOME"] * dask_df["CPI99"]
+dask_df["FAMINC"] = dask_df["FAMINC"] * dask_df["CPI99"]
 
-        # Deixando colunas de household income e family income em dólares de 1999
-        df_i["HHINCOME"] = df_i["HHINCOME"] * df_i["CPI99"]
-        df_i["FAMINC"] = df_i["FAMINC"] * df_i["CPI99"]
+# Dando join com estado
+dask_df = dd.merge(dask_df, state_fips, how="left", on="STATEFIP")
 
-        # Removendo multiplicador da inflação
-        df_i.drop(columns="CPI99", axis=1, inplace=True)
-
-        # Juntando códigos dos estados com o dataframe do cps
-        df_i = df_i.merge(state_fips, how="left", on="STATEFIP")
-        df_final = df_final.append(df_i)
-        print("Chunk {} out of {} succesfully processed!".format(n_chunk, 98))
-        n_chunk += 1
-    else:
-        break
-
-df_final.to_csv("cleaned_data/cleaned_cps.csv", single_file=True)
+# Salvando dataframe
+dask_df.to_csv("cleaned_data/cps_2000_2016_cleaned.csv", single_file=True, index=False)
