ca3491386abd3c1953354828561303728b9a09af
mittelmax <maxmitteldorf@gmail.com>
2020-10-23 00:16:21 -0300

    adicionei informação de área metropolitana nos dataframes da nba

--- Clean_Data/nba_cleaning.py
+++ Clean_Data/nba_cleaning.py
@@ -35,5 +35,24 @@ birthplaces = birthplaces.iloc[:, [0, 1, 3]]
 performance_aggr = pd.merge(performance, birthplaces, how="inner", on="player")
 performance_aggr = pd.merge(performance_aggr, high_schools, how="inner", on=["player", "state"])
 
+# Merging metropolitan area data
+df_fips = pd.read_csv("{}/Data/fips_data.csv".format(download_wd))
+df_final = pd.merge(performance_aggr, df_fips, how="left", left_on=["state", "birthplace"], right_on=["state_id", "city_ascii"])
+df_final.drop(["state_id", "city_ascii"], inplace=True, axis=1)
+df_final.sort_values(["metfips"], inplace=True)
+
+# Checking Na percentage
+percent_missing = df_final.isnull().sum() * 100 / len(df_final)
+missing_value_df = pd.DataFrame({"column_name": df_final.columns, "percent_missing": percent_missing})
+
+# Checking cities/county's with problems
+df_nas_metfips = df_final[df_final["metfips"].isna()]
+df_nas_countyfips = df_final[df_final["county_fips"].isna()]
+df_nas = df_nas_countyfips.append(df_nas_metfips)
+df_nas = df_nas["birthplace"].unique()
+
+# Saving dataframe
+df_final.to_csv("cleaned_data/nba_cleaned")
+
 # Created a database with less columns for a less noisy visualization
 performance_aggr_gmsc = performance_aggr[["year", "player", "gmsc", "birthplace", "state", "hs_city"]]
