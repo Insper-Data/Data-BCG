9e944342f94d4214614692cf1b6ca25d6e1bdaf3
gustavobsales <gustavobsales@gmail.com>
2020-10-28 16:22:17 -0300

    Adicionando joins para dados da área metropolitana

--- Clean_Data/clean_ucr.py
+++ Clean_Data/clean_ucr.py
@@ -5,6 +5,15 @@ from siuba import *
 import re
 import pandas as pd
 
+agencies_data = pd.read_csv("https://raw.githubusercontent.com/jacobkap/crimedatatool_helper/master/data/crosswalk.csv")
+agencies_data = (agencies_data >>
+                 select(_.ori, _.fips_state_county_code) >>
+                 mutate(fips_state_county_code = _.fips_state_county_code.astype(str)))
+fips_data = pd.read_csv("https://raw.githubusercontent.com/Insper-Data/Data_BCG/master/Download_Data/Data/fips_data.csv")
+fips_data = (fips_data >>
+             select(_.county_fips, _.metfips) >>
+             mutate(county_fips = _.county_fips.astype(str)))
+
 ######## Getting File Directory
 def get_filepath(message):
     root = tk.Tk()
@@ -28,17 +37,19 @@ def clean_arrests():
         for df in data:
 
             df = (df >>
-             select(_["ORI", "STATE", "STNAME", "YEAR", "MSA",
-                    "SEQNO", "SUB", "CORE",
+             select(_["ORI", "YEAR", "SUB",
                    "OFFENSE":"JN", "ZERO"]) >>
              filter(_.ZERO == "Not used") >>
              select(-_.ZERO))
+
+            df = df.merge(agencies_data, how="inner", left_on="ORI", right_on="ori")
+            df = df.merge(fips_data, how="inner", left_on="fips_state_county_code", right_on="county_fips")
+            df = df.drop(columns=["ORI", "ori", "fips_state_county_code", "county_fips"])
 
             db = db.append(df)
 
         filename = db.YEAR.iloc[0]
         db.to_csv(os.path.join(db_path, str(filename) + ".csv"))
-        os.remove(file_wd)
 
 def clean_offenses():
     db_path = get_filepath("Selecione a pasta que contém as bases desejadas")
