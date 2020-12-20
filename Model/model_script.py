2a897029a31fc1df002878fadd537699d68157d3
mittelmax <maxmitteldorf@gmail.com>
2020-12-20 16:39:10 -0300

    coloquei lgbm

--- Model/model_script.py
+++ Model/model_script.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
 from siuba import *
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.preprocessing import LabelEncoder
 import tkinter as tk
@@ -12,6 +10,7 @@ from tkinter import filedialog
 import shap
 from boruta import BorutaPy
 import pickle
+import lightgbm as lgb
 
 # # Function that identifies the database's filepath
 # # def get_datapath(message):
@@ -53,62 +52,58 @@ for train_index, test_index in tscv.split(X):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-# Creating random forest model
-rf = RandomForestRegressor(n_estimators=200, random_state=1234, n_jobs=-1, max_depth=10)
-
-# Fitting rf model on training data
-rf.fit(X_train, y_train)
-
-# Predicting on test data
-y_hat_rf = rf.predict(X_test)
-
-# Model performance
-RMSE_rf = np.sqrt(mean_squared_error(y_hat_rf, y_test))
-np.round(RMSE_rf, 2)
-r2_rf = r2_score(y_test, y_hat_rf)
-np.round(r2_rf, 2)
-
-# Boruta
-feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1, perc=1)
-feat_selector.fit(X, y)
-
-# Selected vars
-selected_vars = X.columns[feat_selector.support_].to_list()
-
-# Removed vars
-removed_vars = [var for var in X.columns.values if var not in selected_vars]
-
-# Selecting boruta vars to retrain model
-X_boruta = db[selected_vars].values
-
-for train_index, test_index in tscv.split(X_boruta):
-    X_train, X_test = X_boruta[train_index], X_boruta[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-
-# Refitting model based on boruta vars
-rf.fit(X_train, y_train)
-
-# Analyzing model performance with boruta vars
-y_hat_rf = rf.predict(X_test)
-RMSE_rf_boruta = np.sqrt(mean_squared_error(y_hat_rf, y_test))
-np.round(RMSE_rf_boruta, 2)
-r2_rf_boruta = r2_score(y_test, y_hat_rf)
-np.round(r2_rf_boruta, 2)
-
-# SHAP
-explainer = shap.TreeExplainer(rf)
-shap_values = explainer.shap_values(X_train)
-
-# Making dataframe of shapley values
-shap_df = pd.DataFrame(data=shap_values, columns=selected_vars)
-shap_df = shap_df.mean(axis="rows").abs().sort_values(ascending=False)
-
-# # Visualizing SHAP results
-# shap.summary_plot(shap_values, X_train, plot_type="bar", feature_names=selected_vars)
-# shap.summary_plot(shap_values, X_train, feature_names=selected_vars)
-
-# Saving SHAP results with pickle
-# filename="shap_result_perc_99"
+# # Creating random forest model
+# rf = RandomForestRegressor(n_estimators=200, random_state=1234, n_jobs=-1, max_depth=10)
+#
+# # Fitting rf model on training data
+# rf.fit(X_train, y_train)
+#
+# # Predicting on test data
+# y_hat_rf = rf.predict(X_test)
+#
+# # Model performance
+# RMSE_rf = np.sqrt(mean_squared_error(y_hat_rf, y_test))
+# r2_rf = r2_score(y_test, y_hat_rf)
+#
+# # Boruta
+# feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1, perc=1)
+# feat_selector.fit(X, y)
+#
+# # Selected vars
+# selected_vars = feature_names[feat_selector.support_].to_list()
+#
+# # Removed vars
+# removed_vars = [var for var in feature_names if var not in selected_vars]
+#
+# # Selecting boruta vars to retrain model
+# X_boruta = db[selected_vars].values
+#
+# for train_index, test_index in tscv.split(X_boruta):
+#     X_train, X_test = X_boruta[train_index], X_boruta[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+#
+# # Refitting model based on boruta vars
+# rf.fit(X_train, y_train)
+#
+# # Analyzing model performance with boruta vars
+# y_hat_rf = rf.predict(X_test)
+# RMSE_rf_boruta = np.sqrt(mean_squared_error(y_hat_rf, y_test))
+# r2_rf_boruta = r2_score(y_test, y_hat_rf)
+#
+# # SHAP
+# explainer = shap.TreeExplainer(rf)
+# shap_values = explainer.shap_values(X_train)
+#
+# # Making dataframe of shapley values
+# shap_df = pd.DataFrame(data=shap_values, columns=selected_vars)
+# shap_df = shap_df.mean(axis="rows").abs().sort_values(ascending=False)
+#
+# # # Visualizing SHAP results
+# # shap.summary_plot(shap_values, X_train, plot_type="bar", feature_names=selected_vars)
+# # shap.summary_plot(shap_values, X_train, feature_names=selected_vars)
+#
+# # Saving SHAP results with pickle
+# filename="shap_result_perc_1"
 # outfile = open(filename,'wb')
 # pickle.dump(shap_values, outfile)
 # outfile.close()
@@ -118,40 +113,77 @@ shap_df = shap_df.mean(axis="rows").abs().sort_values(ascending=False)
 # shap_values = pickle.load(infile)
 
 #########################################
-
 # LightGBM
-# Creating dataset
-# lgb_train = lgb.Dataset(X_train, y_train.ravel())
-# lgb_eval = lgb.Dataset(X_test, y_test.ravel(), reference=lgb_train)
-#
-# # specify your configurations as a dict
-# params = {
-#    'boosting_type': 'gbdt',
-#    'objective': 'regression',
-#    'metric': {'l2', 'l1'},
-#    'num_leaves': 31,
-#    'learning_rate': 0.05,
-#    'feature_fraction': 0.9,
-#    'bagging_fraction': 0.8,
-#    'bagging_freq': 5,
-#    'verbose': 0,
-#    'num_threads': 4
-# }
-#
-# print('Starting training...')
-# # train
-# gbm = lgb.train(params,
-#                lgb_train,
-#                num_boost_round=20,
-#                valid_sets=lgb_eval,
-#                early_stopping_rounds=5)
-#
-# print('Saving model...')
-# # save model to file
-# gbm.save_model('model.txt')
-#
-# print('Starting predicting...')
-# # predict
-# y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
-# # eval
-# print('The rmse of prediction is:', mean_squared_error(y_test, y_pred) ** 0.5)
+# Creating datasets
+
+# Boruta
+# Train and test
+lgb_train = lgb.Dataset(X_train, y_train, feature_name=feature_names.tolist(),
+                        categorical_feature=["state_cat", "birthplace_cat"])
+
+# specify your configurations as a dict
+params = {
+   'boosting': 'rf',
+   'objective': 'regression',
+   'metric': 'rmse',
+   'num_leaves': 31,
+   'learning_rate': 0.05,
+   'feature_fraction': 0.9,
+   'bagging_fraction': 0.8,
+   'bagging_freq': 5,
+   'verbose': 1,
+   'num_threads': 4,
+    'num_boost_round': 100
+}
+
+# Initializing regressor
+lgbmreg = lgb.LGBMRegressor(boosting_type="rf", objective="regression", max_iterations=1000, num_leaves=31,
+                            min_data_in_leaf=27, max_depth=-1, learning_rate=0.015, feature_fraction=0.9,
+                            bagging_freq=1, bagging_fraction=0.9, bagging_seed=11, metric="rmse", verbosity=-1,
+                            nthread=4, num_boost_round=100)
+
+# Train
+lgbm_model = lgb.train(params, lgb_train)
+
+# predict
+y_pred_lgb = lgbm_model.predict(X_test, num_iteration=lgbm_model.best_iteration)
+
+# Model performance
+RMSE_lgb = np.sqrt(mean_squared_error(y_pred_lgb, y_test))
+r2_rf = r2_score(y_test, y_pred_lgb)
+
+# # #  Boruta
+# feat_selector = BorutaPy(lgbmreg, n_estimators='auto', verbose=2, random_state=1, perc=90)
+# feat_selector.fit(X, y)
+
+# Selected vars
+selected_vars = feature_names[feat_selector.support_].to_list()
+
+# Selecting boruta vars to retrain model
+X_boruta = db[selected_vars].values
+
+for train_index, test_index in tscv.split(X_boruta):
+    X_train, X_test = X_boruta[train_index], X_boruta[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+# Train and test
+lgb_train = lgb.Dataset(X_train, y_train, feature_name=selected_vars,
+                        categorical_feature=["state_cat", "birthplace_cat"])
+
+# Train
+lgbm_model = lgb.train(params, lgb_train)
+
+# predict test
+y_pred_lgb_test = lgbm_model.predict(X_test, num_iteration=lgbm_model.best_iteration)
+
+# Model performance on test data
+RMSE_lgb_boruta_test = np.sqrt(mean_squared_error(y_pred_lgb_test, y_test))
+r2_rf_boruta_test = r2_score(y_test, y_pred_lgb_test)
+
+# predict train
+y_pred_lgb_train = lgbm_model.predict(X_train, num_iteration=lgbm_model.best_iteration)
+
+# Model performance on training data
+RMSE_lgb_boruta_train = np.sqrt(mean_squared_error(y_pred_lgb_train, y_train))
+r2_rf_boruta_train = r2_score(y_train, y_pred_lgb_train)
+
