286c389ac2af622b4d961fef7f331a95152833b4
Max <59523289+mittelmax@users.noreply.github.com>
2021-01-04 02:27:54 -0300

    adicionei optune e arrumei lgbm

--- Model/model_script.py
+++ Model/model_script.py
@@ -6,12 +6,15 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
+from sklearn.utils.validation import _deprecate_positional_args
 import tkinter as tk
 from tkinter import filedialog
 import shap
 from boruta import BorutaPy
 import pickle
 import lightgbm as lgb
+import optuna
 
 # # Function that identifies the database's filepath
 # def get_datapath(message):
@@ -19,6 +22,9 @@ import lightgbm as lgb
 #     root.withdraw()
 #     return filedialog.askopenfilename(title=message)
 # db = pd.read_csv(get_datapath(message="Select file"))
+
+BORUTA = False
+TUNING = False
 
 # Importing the database
 db = pd.read_csv("pre_model_data/df_2009_2015_feat_engineered9.csv")
@@ -41,141 +47,123 @@ db.drop(["state","birthplace"],axis="columns", inplace=True)
 y = db["gmsc"].ravel()
 X = db.drop("gmsc", axis="columns")
 X_copy = X[:]
-X.drop("date", axis="columns", inplace=True)
 
 # Saving feature names for later use
 feature_names = X.columns
 
-# Transforming in to array
+# Trasforming X into an array
 X = X.values
 
-# Time-consistent train and test split
-tscv = TimeSeriesSplit()
-for train_index, test_index in tscv.split(X):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+if BORUTA:
+    rf = RandomForestRegressor(n_estimators=200, random_state=1234, n_jobs=-1, max_depth=10)
 
-# # Creating random forest model
-# rf = RandomForestRegressor(n_estimators=200, random_state=1234, n_jobs=-1, max_depth=10)
-#
-# # Fitting rf model on training data
-# rf.fit(X_train, y_train)
-#
-# # Predicting on test data
-# y_hat_rf = rf.predict(X_test)
-#
-# # Model performance
-# RMSE_rf = np.sqrt(mean_squared_error(y_hat_rf, y_test))
-# r2_rf = r2_score(y_test, y_hat_rf)
-#
-# # Boruta
-# feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1, perc=1)
-# feat_selector.fit(X, y)
-#
-# # Selected vars
-# selected_vars = feature_names[feat_selector.support_].to_list()
-#
-# # Removed vars
-# removed_vars = [var for var in feature_names if var not in selected_vars]
-#
-# # Selecting boruta vars to retrain model
-# X_boruta = db[selected_vars].values
-#
-# for train_index, test_index in tscv.split(X_boruta):
-#     X_train, X_test = X_boruta[train_index], X_boruta[test_index]
-#     y_train, y_test = y[train_index], y[test_index]
-#
-# # Refitting model based on boruta vars
-# rf.fit(X_train, y_train)
-#
-# # Analyzing model performance with boruta vars
-# y_hat_rf = rf.predict(X_test)
-# RMSE_rf_boruta = np.sqrt(mean_squared_error(y_hat_rf, y_test))
-# r2_rf_boruta = r2_score(y_test, y_hat_rf)
-#
-# # SHAP
-# explainer = shap.TreeExplainer(rf)
-# shap_values = explainer.shap_values(X_train)
-#
-# # Making dataframe of shapley values
-# shap_df = pd.DataFrame(data=shap_values, columns=selected_vars)
-# shap_df = shap_df.mean(axis="rows").abs().sort_values(ascending=False)
-#
-# # # Visualizing SHAP results
-# # shap.summary_plot(shap_values, X_train, plot_type="bar", feature_names=selected_vars)
-# # shap.summary_plot(shap_values, X_train, feature_names=selected_vars)
-#
-# # Saving SHAP results with pickle
-# filename="shap_result_perc_1"
-# outfile = open(filename,'wb')
-# pickle.dump(shap_values, outfile)
-# outfile.close()
+    # Boruta
+    feat_selector = BorutaPy(rf, n_estimators="auto", verbose=2, random_state=1, perc=70)
+    feat_selector.fit(X, y)
 
-# # Opening SHAP results with pickle
-# infile = open("shap_result", "rb")
-# shap_values = pickle.load(infile)
+    # Selected vars
+    boruta_vars = feature_names[feat_selector.support_].to_list()
 
-#########################################
-# LightGBM
+    # Removed vars
+    removed_vars = [var for var in feature_names if var not in selected_vars]
 
+    # Saving Boruta vars
+    filename="boruta_perc_70"
+    outfile = open(filename,'wb')
+    pickle.dump(boruta_vars, outfile)
+    outfile.close()
+
+# #########################################
+# # LightGBM
+
+if not BORUTA:
+    # Reading Boruta vars
+    infile = open("boruta_perc_70", "rb")
+    boruta_vars = pickle.load(infile)
+
+if TUNING:
+    cv = TimeSeriesSplit()
+
+    ## Objective Function
+    def objective(trial, cv=cv, cv_fold_func=np.average):
+        params = {
+            'objective': 'regression',
+            'metric': 'rmse',
+            'boosting': 'rf',
+            'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
+            'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
+            'num_leaves': trial.suggest_int('num_leaves', 2, 200),
+            'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
+            'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
+            'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
+            'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
+            'max_depth': trial.suggest_int('max_depth', 5, 15),
+            'learning_rate': trial.suggest_uniform('learning_rate', 0.01, 0.10)}
+
+        # ffitting and returning RMSE scores
+        rmse_list = []
+        for i, (train_idx, test_idx) in enumerate(tscv.split(X)):
+            train_x, valid_x = X[train_idx], X[test_idx]
+            train_y, valid_y = y[train_idx], y[test_idx]
+
+            train_data = lgb.Dataset(train_x, label=train_y)
+
+            model = lgb.train(params, train_data)
+            pred = model.predict(valid_x)
+            rmse = np.sqrt(mean_squared_error(pred, valid_y))
+            rmse_list.append(rmse)
+
+        print("Trial done: RMSE values on folds: {}".format(rmse_list))
+        return cv_fold_func(rmse_list)
+
+    ## Optuna results
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=80)
+    print("Number of finished trials:", len(study.trials))
+    print("Best trial:", study.best_trial.params)
+    best_params = study.best_trial.params
+
+    # Building parameter dictionary
+    fixed_params = {'objective': 'regression',
+                    'metric': 'rmse',
+                    'boosting': 'gbdt',
+                    'num_threads': 4}
+
+    best_params = {**best_params, **fixed_params}
+
+    ## Saving best params
+    filename="best_params"
+    outfile = open(filename,"wb")
+    pickle.dump(best_params, outfile)
+    outfile.close()
+
+if not TUNING:
+    # Reading best_params
+    infile = open("best_params", "rb")
+    best_params = pickle.load(infile)
+
+
+# Fitting lgbm on tuned parameters
 # Creating datasets
-# Train and test
-lgb_train = lgb.Dataset(X_train, y_train, feature_name=feature_names.tolist(),
-                        categorical_feature=["state_cat", "birthplace_cat"])
+X_boruta = X_copy[boruta_vars]
+X_boruta = X_boruta.values
 
-# specify your configurations as a dict
-params = {
-   'boosting': 'rf',
-   'objective': 'regression',
-   'metric': 'rmse',
-   'num_leaves': 31,
-   'learning_rate': 0.05,
-   'feature_fraction': 0.9,
-   'bagging_fraction': 0.8,
-   'bagging_freq': 5,
-   'verbose': 1,
-   'num_threads': 4,
-    'num_boost_round': 100
-}
-
-# Initializing regressor
-lgbmreg = lgb.LGBMRegressor(boosting_type="rf", objective="regression", max_iterations=1000, num_leaves=31,
-                            min_data_in_leaf=27, max_depth=-1, learning_rate=0.015, feature_fraction=0.9,
-                            bagging_freq=1, bagging_fraction=0.9, bagging_seed=11, metric="rmse", verbosity=-1,
-                            nthread=4, num_boost_round=100)
-
-# Train
-lgbm_model = lgb.train(params, lgb_train)
-
-# predict
-y_pred_lgb = lgbm_model.predict(X_test, num_iteration=lgbm_model.best_iteration)
-
-# Model performance
-RMSE_lgb = np.sqrt(mean_squared_error(y_pred_lgb, y_test))
-r2_rf = r2_score(y_test, y_pred_lgb)
-
-# Boruta
-feat_selector = BorutaPy(lgbmreg, n_estimators='auto', verbose=2, random_state=1, perc=90)
-feat_selector.fit(X, y)
-
-# Selected vars
-selected_vars = feature_names[feat_selector.support_].to_list()
-
-# Selecting boruta vars to retrain model
-X_boruta = db[selected_vars]
-
+# Time series split
+tscv = TimeSeriesSplit()
 for train_index, test_index in tscv.split(X_boruta):
-    X_train, X_test = X_boruta.values[train_index], X_boruta.values[test_index]
+    X_train, X_test = X_boruta[train_index], X_boruta[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-# Train and test
-lgb_train = lgb.Dataset(X_train, y_train, feature_name=selected_vars,
-                        categorical_feature=["state_cat", "birthplace_cat"])
 
-# Train
-lgbm_model = lgb.train(params, lgb_train)
+# Training and eval sets
+lgb_train = lgb.Dataset(X_train, y_train, feature_name=boruta_vars, categorical_feature=["birthplace_cat"])
+lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
 
-# predict test
+# Training model with best params
+lgbm_model = lgb.train(best_params, lgb_train, 250, valid_sets=lgb_eval,
+                 early_stopping_rounds=50, verbose_eval=20)
+
+# predicting on test data
 y_pred_lgb_test = lgbm_model.predict(X_test, num_iteration=lgbm_model.best_iteration)
 
 # Model performance on test data
@@ -189,72 +177,72 @@ y_pred_lgb_train = lgbm_model.predict(X_train, num_iteration=lgbm_model.best_ite
 RMSE_lgb_boruta_train = np.sqrt(mean_squared_error(y_pred_lgb_train, y_train))
 r2_rf_boruta_train = r2_score(y_train, y_pred_lgb_train)
 
-# Analyzing model performance over time
-X_boruta["game_id"] = X_copy["game_id"]
-X_boruta["year"] = X_copy["year"]
-
-# Experimental (nunca usei esse isin)
-
-valid_game_ids = (X_boruta >>
-    count(_.game_id) >>
-    filter(_.n >= 30)).game_id
-
-X_boruta = X_boruta[X_boruta.game_id.isin(valid_game_ids)]
-
-## Se não funcionar
-
+# # Analyzing model performance over time
+# X_boruta["game_id"] = X_copy["game_id"]
+# X_boruta["year"] = X_copy["year"]
+#
+# # Experimental (nunca usei esse isin)
+#
 # valid_game_ids = (X_boruta >>
 #     count(_.game_id) >>
-#     filter(_.n >= 30))
+#     filter(_.n >= 30)).game_id
 #
-# X_boruta = (X_boruta >>
-#                 semi_join(valid_game_ids, on="game_id"))
-
-def train_test(X, y, id):
-    train_index = X.game_id <= id
-    test_id = sorted(list(set(X[X.game_id > id].game_id)))[0]
-    test_index = X.game_id == test_id
-    X_train = X[train_index].reset_index().drop(["game_id", "index", "year"], axis="columns")
-    X_train = X_train.values
-    y_train = y[train_index]
-    X_test = X[test_index].reset_index().drop(["game_id", "index", "year"], axis="columns").values
-    y_test = y[test_index]
-    return X_train, y_train, X_test, y_test
-
-dic = {}
-
-for year in sorted(list(set(X_boruta.year))):
-    for id in sorted(list(set(X_boruta.game_id)))[:len(set(X_boruta.game_id)) - 1]:
-    X_train, y_train, X_test, y_test = train_test(X_boruta.loc[X_boruta.year == year, :], y, id)
-
-    # Train and test
-    lgb_train = lgb.Dataset(X_train, y_train, feature_name=selected_vars,
-                            categorical_feature=["state_cat", "birthplace_cat"])
-
-    # Train
-    lgbm_model = lgb.train(params, lgb_train)
-
-    # predict test
-    y_pred_lgb_test = lgbm_model.predict(X_test, num_iteration=lgbm_model.best_iteration)
-
-    # Model performance on test data
-    RMSE_lgb_boruta_test = np.sqrt(mean_squared_error(y_pred_lgb_test, y_test))
-    r2_rf_boruta_test = r2_score(y_test, y_pred_lgb_test)
-
-    # predict train
-    y_pred_lgb_train = lgbm_model.predict(X_train, num_iteration=lgbm_model.best_iteration)
-
-    # Model performance on training data
-    RMSE_lgb_boruta_train = np.sqrt(mean_squared_error(y_pred_lgb_train, y_train))
-    r2_rf_boruta_train = r2_score(y_train, y_pred_lgb_train)
-
-    dic[str(id)] = (RMSE_lgb_boruta_test, r2_rf_boruta_test, RMSE_lgb_boruta_train, r2_rf_boruta_train, year)
-    print(id)
-
-# # Saving lgbm dict results with pickle
-filename="lgbm_dict_gameid"
-outfile = open(filename,'wb')
-pickle.dump(dic, outfile)
-outfile.close()
-
-n_ids = X_boruta >> count(_.game_id)
+# X_boruta = X_boruta[X_boruta.game_id.isin(valid_game_ids)]
+#
+# ## Se não funcionar
+#
+# # valid_game_ids = (X_boruta >>
+# #     count(_.game_id) >>
+# #     filter(_.n >= 30))
+# #
+# # X_boruta = (X_boruta >>
+# #                 semi_join(valid_game_ids, on="game_id"))
+#
+# def train_test(X, y, id):
+#     train_index = X.game_id <= id
+#     test_id = sorted(list(set(X[X.game_id > id].game_id)))[0]
+#     test_index = X.game_id == test_id
+#     X_train = X[train_index].reset_index().drop(["game_id", "index", "year"], axis="columns")
+#     X_train = X_train.values
+#     y_train = y[train_index]
+#     X_test = X[test_index].reset_index().drop(["game_id", "index", "year"], axis="columns").values
+#     y_test = y[test_index]
+#     return X_train, y_train, X_test, y_test
+#
+# dic = {}
+#
+# for year in sorted(list(set(X_boruta.year))):
+#     for id in sorted(list(set(X_boruta.game_id)))[:len(set(X_boruta.game_id)) - 1]:
+#         X_train, y_train, X_test, y_test = train_test(X_boruta.loc[X_boruta.year == year, :], y, id)
+#
+#         # Train and test
+#         lgb_train = lgb.Dataset(X_train, y_train, feature_name=selected_vars,
+#                                 categorical_feature=["state_cat", "birthplace_cat"])
+#
+#         # Train
+#         lgbm_model = lgb.train(params, lgb_train)
+#
+#         # predict test
+#         y_pred_lgb_test = lgbm_model.predict(X_test, num_iteration=lgbm_model.best_iteration)
+#
+#         # Model performance on test data
+#         RMSE_lgb_boruta_test = np.sqrt(mean_squared_error(y_pred_lgb_test, y_test))
+#         r2_rf_boruta_test = r2_score(y_test, y_pred_lgb_test)
+#
+#         # predict train
+#         y_pred_lgb_train = lgbm_model.predict(X_train, num_iteration=lgbm_model.best_iteration)
+#
+#         # Model performance on training data
+#         RMSE_lgb_boruta_train = np.sqrt(mean_squared_error(y_pred_lgb_train, y_train))
+#         r2_rf_boruta_train = r2_score(y_train, y_pred_lgb_train)
+#
+#         dic[str(id)] = (RMSE_lgb_boruta_test, r2_rf_boruta_test, RMSE_lgb_boruta_train, r2_rf_boruta_train, year)
+#         print(id)
+#
+# # # Saving lgbm dict results with pickle
+# filename="lgbm_dict_gameid"
+# outfile = open(filename,'wb')
+# pickle.dump(dic, outfile)
+# outfile.close()
+# n_ids = X_boruta >> count(_.game_id)
+#
